11714d295c9f73986795249b63111f530934dcf2
rcrdgrc <ricktx04@example.com>
2021-03-09 12:51:40 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hello Dojo!</h1>
+        <h2>Things I need to do:</h2>
+        <p style={{width: "200px"}}>
+          <ul>
+            <li>Learn React</li>
+            <li>Climb Mt. Everest</li>
+            <li>Run a marathon</li>
+            <li>Feed the dogs</li>
+          </ul>
+        </p>
+    </div>
+  );
+}
+
+export default App;
